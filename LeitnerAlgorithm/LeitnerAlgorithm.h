3c66e74784782a34c8e79679fca902125fdc0a2d
mfsaglam <mfsaglam1@gmail.com>
2024-09-08 16:17:11 +0300

    Initial Commit

---
+++ LeitnerAlgorithm/LeitnerAlgorithm.h
@@ -0,0 +1,18 @@
+//
+//  LeitnerAlgorithm.h
+//  LeitnerAlgorithm
+//
+//  Created by Fatih Sağlam on 8.09.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeitnerAlgorithm.
+FOUNDATION_EXPORT double LeitnerAlgorithmVersionNumber;
+
+//! Project version string for LeitnerAlgorithm.
+FOUNDATION_EXPORT const unsigned char LeitnerAlgorithmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeitnerAlgorithm/PublicHeader.h>
+
+
